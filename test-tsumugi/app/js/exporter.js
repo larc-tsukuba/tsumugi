d0423d42784766ad77cf368f0e6f3f36e37bab5b
Akihiro Kuno <akikuno@users.noreply.github.com>
2025-03-01 13:57:22 +0900

    Add `js/exporter.js`

---
+++ test-tsumugi/app/js/exporter.js
@@ -0,0 +1,52 @@
+// --------------------------------------------------------
+// PNG Exporter
+// --------------------------------------------------------
+
+function exportGraphAsPNG() {
+    const pngContent = cy.png({
+        scale: 6.25,   // Scale to achieve 600 DPI
+        full: true     // Set to true to include the entire graph, even the offscreen parts
+    });
+
+    const a = document.createElement('a');
+    a.href = pngContent;
+    a.download = 'TSUMUGI_XXX_genesymbol.png';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+
+// --------------------------------------------------------
+// CSV Exporter
+// --------------------------------------------------------
+
+function exportGraphAsCSV() {
+    // calculateConnectedComponentsを利用して連結成分を取得
+    const connected_component = calculateConnectedComponents();
+
+    // CSVのヘッダー行
+    let csvContent = "cluster,gene,phenotypes\n";
+
+    // クラスター番号を割り当てて、CSVフォーマットに変換
+    connected_component.forEach((component, clusterIndex) => {
+        const clusterNumber = clusterIndex + 1;
+
+        Object.keys(component).forEach(gene => {
+            const phenotypes = component[gene].join(";"); // 表現型をセミコロン区切りで結合
+
+            // CSVの各行を生成
+            csvContent += `${clusterNumber},${gene},"${phenotypes}"\n`;
+        });
+    });
+
+    // CSVファイルを生成しダウンロード
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'TSUMUGI_XXX_genesymbol.csv';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
